264406eb82486a4081f35012bebabb1b8beef31e
muryoimpl <muryoimpl@gmail.com>
2016-03-01 02:12:38 +0900

    Add Buoys::Buoy's test

---
+++ test/buoys_buoy_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class BuoysBuoyTest < ActiveSupport::TestCase
+  setup do
+    @view_context = ActionView::Base.new
+    Buoys::Loader.load_buoys_files
+  end
+
+  test 'can interpret multiple `link`s in one buoy block with argument' do
+    links = Buoys::Buoy.new(@view_context, :help, true).links
+
+    assert_equal links.size, 2
+    assert_equal links[0].text, 'help'
+    assert_equal links[0].url,  'https://example.com/help'
+    assert_equal links[1].text, 'usage'
+    assert_equal links[1].url,  'https://example.com/help/usage'
+
+    links = Buoys::Buoy.new(@view_context, :help, false).links
+
+    assert_equal links[0].url,  'http://example.com/help'
+    assert_equal links[1].url,  'http://example.com/help/usage'
+  end
+end
