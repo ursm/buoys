77907bf79b2bb48fcc5dac90002d40267bed294b
muryoimpl <muryoimpl@gmail.com>
2016-03-04 00:38:26 +0900

    Add methods to Buoys::Link class
    To judge and decide the Link is the end of breadcrumbs

--- lib/buoys/link.rb
+++ lib/buoys/link.rb
@@ -1,9 +1,18 @@
 module Buoys
   class Link
     attr_accessor :text, :url, :options
+    attr_reader :current
 
     def initialize(text, url, options)
       @text, @url, @options = text, url, options
+    end
+
+    def mark_as_current!
+      @current = true
+    end
+
+    def current?
+      !!@current
     end
   end
 end
