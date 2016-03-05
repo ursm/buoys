e51f6f3fa1c7824acdb7c55745d8847e2bc711b2
muryoimpl <muryoimpl@gmail.com>
2016-03-06 04:17:04 +0900

    Redefine url and url= methods
    To return '' when Buoys::Link#current is true

--- lib/buoys/link.rb
+++ lib/buoys/link.rb
@@ -1,10 +1,10 @@
 module Buoys
   class Link
-    attr_accessor :text, :url, :options
+    attr_accessor :text, :options
     attr_reader :current
 
     def initialize(text, url, options)
-      @text, @url, @options = text, url, options
+      @text, @_url, @options = text, url, options
     end
 
     def mark_as_current!
@@ -13,6 +13,14 @@ module Buoys
 
     def current?
       !!@current
+    end
+
+    def url
+      current? ? '' : @_url
+    end
+
+    def url=(str)
+      @_url = str
     end
   end
 end
