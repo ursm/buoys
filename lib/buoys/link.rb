37c1e59e01c1ecfa6a09cf1d8ec786cfb5a4f386
muryoimpl <muryoimpl@gmail.com>
2016-03-07 00:33:31 +0900

    Use Buoys::Config class to treat configurations

--- lib/buoys/link.rb
+++ lib/buoys/link.rb
@@ -3,12 +3,17 @@ module Buoys
     attr_accessor :text, :options
     attr_reader :current
 
+    CONFIG = {
+      current_class: (Buoys::Config.current_class || 'current'),
+      link_current: (Buoys::Config.link_current || false),
+    }.with_indifferent_access
+
     def initialize(text, url, options)
-      @text, @_url, @options = text, url, options
+      @text, @_url, @options = text, url, options.with_indifferent_access
     end
 
     def mark_as_current!
-      options.deep_merge!(class: 'current')
+      options.deep_merge!(class: config[:current_class])
       @current = true
     end
 
@@ -22,6 +27,15 @@ module Buoys
 
     def url=(str)
       @_url = str
+    end
+
+    private
+
+    def config
+      override_options = (options.keys & CONFIG.keys).each_with_object({}) {|key, hash|
+        hash[key] = options[key]
+      }
+      CONFIG.merge(override_options)
     end
   end
 end
