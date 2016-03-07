52e09b65468dff4977ee392a8e57e3deef061a82
muryoimpl <muryoimpl@gmail.com>
2016-03-07 22:24:24 +0900

    Add generators task `buoys:install`

---
+++ lib/generators/buoys/templates/breadcrumbs.rb
@@ -0,0 +1,38 @@
+buoy :example do
+  link :example, 'http://localhost:3000'
+end
+
+# buoy :stories do
+#   link 'Stories', stories_path
+# end
+
+# `crumb` is the alias of `buoy`.
+# ex)
+# crumb :stories do
+#   link 'Stories', stories_path
+# end
+
+# If you use symbol as link's first argument, it is used as I18n key.
+# The key is searched in the scope of 'buoys.breadcrumbs'.
+#
+# ex)
+# buoy :story do |story|
+#   link :story, story_path(story)
+#   # same as `link I18n.t('buoys.breadcrumbs.story'), story_path(story)``
+# end
+
+# You can alse override Buoys configuration
+#
+# ex)
+# buoy :story_tasks do |story|
+#   link :story_tasks, story_tasks_path
+#   pre_buoy :story, story, {link_current: true}
+# end
+
+# You can use `pre_buoy` as parent. `parent` is the alias of `pre_buoy`
+#
+# ex)
+# buoy :story_tasks do |story|
+#   link :story_tasks, story_tasks_path
+#   parent :story, story
+# end
