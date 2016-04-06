4b3d19c890a4e57ab68cd63271bee8f5fd492d77
Hiroshi Yoshida <hrysd22@gmail.com>
2016-04-06 17:03:19 +0900

    Do not drop args

--- test/dummy/config/buoys/buoys.rb
+++ test/dummy/config/buoys/buoys.rb
@@ -10,3 +10,8 @@ buoy :show_books do |number|
   parent :books
   link "show-#{number}", "http://example.com/books/#{number}", link_current: true
 end
+
+buoy :edit_book_author do |book_id, author_id|
+  parent :books
+  link "edit-#{book_id}-#{author_id}", "http://example.com/books/#{book_id}/authors/#{author_id}", link_current: true
+end
