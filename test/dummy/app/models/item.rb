66c6cd05c8cc48f70a9ee478e00f97b3938b2d25
Ken Muryoi <muryoimpl@gmail.com>
2021-01-16 16:49:00 +0900

    Update gem dependencies (#64)
    * Update dependent rails gem to 5.2
    
    * Update bundler
    
    * Re-create dummy environment with Rails 5.2
    
    * Replace command "bundle exec rake" with "bin/test"
    
    * Fix offence

--- test/dummy/app/models/item.rb
+++ test/dummy/app/models/item.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-class Item < ActiveRecord::Base
+class Item < ApplicationRecord
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'about'         => 'dummy#dummy', as: :about
   get 'about/history' => 'dummy#dummy', as: :history
 
